04a89343d40317883477ba971bdc41b820884171
Samuel Scherer <scheresr@mail.uc.edu>
2019-09-21 11:00:58 -0400

    Created basic editing scene with crappy movement scripts

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float movementDeadzone = .1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get Movement
+        var movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (Mathf.Abs(movementInput.x) > movementDeadzone || Mathf.Abs(movementInput.y) > movementDeadzone)
+        {
+            // Vector magnitude won't exceed 1
+            if(movementInput.magnitude > 1)
+            {
+                movementInput.Normalize();
+            }
+
+            // Send to playerbehavior
+            GetComponent<PlayerBehavior>().Move(movementInput);
+        }
+    }
+}
